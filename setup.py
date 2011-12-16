495938082cb5e28006eab87ec6a5256d5da2a0a8
Ryan Kelly <rkelly@mozilla.com>
2011-12-16 14:31:48 +1100

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['M2Crypto']
 
 setup(name='PyVEP',
-      version='0.2.0',
+      version='0.2.1',
       description='Python library for the Verified Email Protocol',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
