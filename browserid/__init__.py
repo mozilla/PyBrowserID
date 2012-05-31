78bdd943df505b2dac383c468cf24a4165980d62
Alexis Metaireau <alexis@notmyidea.org>
2012-05-31 17:03:03 +0200

    prepare next release

--- browserid/__init__.py
+++ browserid/__init__.py
@@ -8,7 +8,7 @@ Python library for the Verified Email Protocol.
 """
 
 __ver_major__ = 0
-__ver_minor__ = 6
+__ver_minor__ = 7
 __ver_patch__ = 0
 __ver_sub__ = ""
 __ver_tuple__ = (__ver_major__, __ver_minor__, __ver_patch__, __ver_sub__)
